38234e6e06eda798441fdf2f2ae3501eea50cefe
Joacomontes <Joacotemonts89@gmail.com>
2025-05-18 11:42:00 -0300

    update

--- database/connectionSQLServer.js
+++ database/connectionSQLServer.js
@@ -2,10 +2,8 @@ const sql = require('mssql');
 
 // Configuración de conexión
 const config = {
-    user: 'TU_USUARIO',
-    password: 'TU_CONTRASEÑA',
-    server: 'TU_SERVIDOR', // puede ser 'localhost' o una IP
-    database: 'TU_BASE_DE_DATOS',
+    server: 'localhost',
+    database: 'node_restapi',
     options: {
         encrypt: true, // true si usás Azure o certificados SSL
         trustServerCertificate: true // true si estás en local sin SSL
